d0e4b181c3441636e9311f225eac2ccaab1a588a
Joe Begley <joebegley15@gmail.com>
2018-09-01 13:26:13 -0400

    Comments added for clarity

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
+// QuickSort function
 const quickSort = function(arr) {
-
 	if (arr.length <= 1) {
 		return arr;
 	}
-
 	const pointer = arr[0], less = [], more = [];
-
-
 	for (var i = 1; i < arr.length; i++) {
 		if (arr[i] < pointer) {
 			less.push(arr[i]);
@@ -14,11 +11,9 @@ const quickSort = function(arr) {
 			more.push(arr[i]);
 		}
 	}
-
-
-
 	return quickSort(less).concat(pointer,quickSort(more))
 }
+// Test for the quicksort function. Inserts 100 numbers from 0-100 into an array and sorts them.
 const arr = [];
 for (var i = 0; i < 100; i++) {
 	arr.push(Math.random() * 100);
